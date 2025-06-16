393f71b4119e4edf2f8327b6d59adfc7bc67feba
Mohit349412 <81349135+Mohit349412@users.noreply.github.com>
2025-06-16 18:04:13 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
 echo "Hello $INPUT_MYINPUT"
-memory=$(cat /proc/meminfo)
-echo "::set-output name=memory::$memory"
-
-#!/bin/sh
-
-echo "Hello $INPUT_MYINPUT"
 mem_kb=$(ps -o rss= -p $$)
 echo "::set-output name=memory_used_kb::$mem_kb"
